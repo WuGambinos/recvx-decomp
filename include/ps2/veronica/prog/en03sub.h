c048147ababca0cf9c21af23735fb4eca5c032f1
Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
2026-08-04 00:46:10 +0200

    Decompile `en03sub` (#55)

--- include/ps2/veronica/prog/en03sub.h
+++ include/ps2/veronica/prog/en03sub.h
@@ -2,7 +2,15 @@
 #define _EN03SUB_H_
 
 #include "types.h"
+#include "macros.h"
 
-
+void bhEne03s(BH_PWORK* epw);
+void bhEne03s_Init(BH_PWORK* epw);
+void bhEne03s_Move(BH_PWORK* epw);
+void bhEne03s_MV00(BH_PWORK* epw);
+void bhEne03s_Dummy(BH_PWORK* epw);
+void bhEne03s_WallCheck(BH_PWORK* epw);
+void bhEne03s_WallCheck2(BH_PWORK* epw);
+void bhEne03s_InitCollision(BH_PWORK* epw);
 
 #endif

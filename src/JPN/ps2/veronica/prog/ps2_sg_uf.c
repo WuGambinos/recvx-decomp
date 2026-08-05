e7cb549138c44abbcc41932f4011de50456fc4f7
Frozen Burnside <frozenburnside@hotmail.com>
2026-08-05 12:31:48 -0300

    Decompile ps2_sg_uf

--- src/JPN/ps2/veronica/prog/ps2_sg_uf.c
+++ src/JPN/ps2/veronica/prog/ps2_sg_uf.c
@@ -1,15 +1,43 @@
-#include "common.h"
+#include "../../../JPN/ps2/veronica/prog/ps2_sg_uf.h"
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufAutoSynch);
+// 100% matching!
+Bool ufAutoSynch(Uint32 count) 
+{
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufChangeWorkBuffer);
+}
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufExitSystem);
+// 100% matching!
+Bool ufChangeWorkBuffer(void* work1, void* work2, void* work3, void* work4) 
+{
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufInitSystem);
+}
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufPlayTime);
+// 100% matching!
+void ufExitSystem(void) 
+{
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufReadAllHead);
+}
 
-INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufSynch);
+// 100% matching!
+Bool ufInitSystem(void* work1, void* work2, void* work3, void* work4) 
+{
+
+}
+
+// 100% matching!
+Sint32 ufPlayTime(Uint8 mode, Uint8 est, Uint8 carry, Uint8 param[32]) 
+{
+
+}
+
+// 100% matching!
+Bool ufReadAllHead(Uint8* ProdNum, UFS_AllPlayHistory* buff) 
+{
+
+}
+
+// 100% matching!
+Bool ufSynch(void) 
+{
+
+}

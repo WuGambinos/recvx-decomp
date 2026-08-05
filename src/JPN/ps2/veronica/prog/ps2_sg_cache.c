1fa868b5fd8863912d27d750d222c906d5f37e65
Frozen Burnside <frozenburnside@hotmail.com>
2026-08-04 22:10:24 -0300

    Add support for Japanese ROM (experimental)

---
+++ src/JPN/ps2/veronica/prog/ps2_sg_cache.c
@@ -0,0 +1,3 @@
+#include "common.h"
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_cache", syCacheOCWB);
---
+++ src/JPN/ps2/veronica/prog/ps2_sg_uf.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufAutoSynch);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufChangeWorkBuffer);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufExitSystem);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufInitSystem);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufPlayTime);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufReadAllHead);
+
+INCLUDE_ASM("build/expected/asm/JPN/ps2/veronica/prog/ps2_sg_uf", ufSynch);

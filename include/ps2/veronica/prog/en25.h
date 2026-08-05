ced44080511d07b1eec2a72fa5022c6d2b7879bf
Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
2026-08-05 13:23:12 +0200

    Decompile `en25` (#56)

--- include/ps2/veronica/prog/en25.h
+++ include/ps2/veronica/prog/en25.h
@@ -2,18 +2,19 @@
 #define _EN25_H_
 
 #include "types.h"
+#include "macros.h"
 
 void bhEne25(BH_PWORK* epw);
 void bhEne25_Init(BH_PWORK* epw);
-void bhEne25_Brain();
+void bhEne25_Brain(BH_PWORK* epw);
 void bhEne25_Move(BH_PWORK* epw);
 void bhEne25_MV00(BH_PWORK* epw);
 void bhEne25_MV01(BH_PWORK* epw);
 void bhEne25_MV02(BH_PWORK* epw);
 void bhEne25_MV03(BH_PWORK* epw);
-void bhEne25_Nage();
-void bhEne25_Damage();
-void bhEne25_Die();
+void bhEne25_Nage(BH_PWORK* epw);
+void bhEne25_Damage(BH_PWORK* epw);
+void bhEne25_Die(BH_PWORK* epw);
 void bhEne25_SetEffect(BH_PWORK* epw);
 void bhEne25_PlayerControl(BH_PWORK* epw);
 

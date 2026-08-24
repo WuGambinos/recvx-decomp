efcbd81df490b4623ffd7478cfbb7bf54f8f3e93
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-08-24 18:23:45 +0200

    decompile `en26` (#61)
    * decompile `bhEne26_MV02`
    
    * decompile `bhEne26_SePlay`
    
    Co-authored-by: Frozen Burnside <frozenburnside@hotmail.com>
    
    * decompile `bhEne26_CheckExpHead`
    
    * decompile `bhEne26_PlayerControl`
    
    * decompile `bhEne26_DG15`
    
    * decompile `bhEne26_DG03`
    
    * decompile `bhEne26_DG04`
    
    * decompile `bhEne26_MV16`
    
    * decompile `bhEne26_SearchNeck`
    
    * decompile `bhEne26_NeckBloodEffect`
    
    * update `en26.c`
    
    * decompile `bhEne26_MV05`
    
    Co-Authored-By: Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
    
    * decompile `bhEne26_MV06`
    
    * decompile `bhEne26_MV07`
    
    * decompile `bhEne26_MV03`
    
    * decompile `bhEne26_MV00`
    
    * decompile `bhEne26_MVType`
    
    * decompile `bhEne26_Move`
    
    * decompile `bhEne26_CalcEnemy`
    
    * decompile `bhEne26_Brain00`
    
    * decompile `bhEne26_CollCheck`
    
    * decompile `bhEne26_DG00`
    
    * decompile `bhEne26_DG01`
    
    * decompile `bhEne26_MV01`
    
    * decompile `bhEne26_Damage`
    
    * decompile `bhEne26_DGType`
    
    * decompile `bhEne26_DG02`
    
    * decompile `bhEne26_ActionModeCheck`
    
    * decompile `bhEne26`
    
    * decompile `bhEne26_CollCheckWall`
    
    * decompile `bhEne26_Init` + migrate part of data
    
    * decompile `bhEne26_DD00`
    
    * decompile `bhEne26_DDType`
    
    * decompile `bhEne26_Die`
    
    * decompile `bhEne26_MainLoop`
    
    * decompile `bhEne26_Nage`
    
    * decompile `bhEne26_NGType`
    
    * decompile `bhEne26_EneSearch`
    
    * decompile `bhEne26_EatCheck`
    
    * decompile `bhEne26_ExpHeadEffect`
    
    * decompile `bhEne26_NG00` (99.95%)
    
    * decompile `bhEne26_PlyDG00`
    
    * decompile `bhEne26_SetMtn`
    
    * decompile `bhEne26_SetBlood`
    
    * update `en26.h`
    
    * decompile `bhEne26_DmgCheckType00`
    
    * update `en26.h`
    
    * decompile `bhEne26_DmgCheck`
    
    * decompile `bhEne26_LinkFireEffect`
    
    * update `en26.h`
    
    * decompile `bhEne26_PlayerLink`
    
    Co-Authored-By: Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>
    
    * decompile `bhEne26_DamageAdd`
    
    * decompile `bhEne26_KamiEffect`
    
    * update `en26.h`
    
    * decompile `bhEne26_CheckMtnTbl`
    
    * update `en26.c`
    
    * apply suggestions
    
    Co-authored-by: Frozen Burnside <frozenburnside@hotmail.com>
    
    * update `en26.h`
    
    * apply suggestions
    
    Co-authored-by: Frozen Burnside <frozenburnside@hotmail.com>
    
    ---------
    
    Co-Authored-By: Frozen Burnside <frozenburnside@hotmail.com>
    Co-Authored-By: Norgesnerd <5824576+Norgesnerd@users.noreply.github.com>

--- include/ps2/veronica/prog/en26.h
+++ include/ps2/veronica/prog/en26.h
@@ -2,14 +2,72 @@
 #define _EN26_H_
 
 #include "types.h"
+#include "macros.h"
+
+typedef struct EN26_WPNDAMAGE_WORK
+{
+    // total size: 0x1C
+    int flg;        // offset 0x0, size 0x4
+    int nm_act;     // offset 0x4, size 0x4
+    int nm_blood;   // offset 0x8, size 0x4
+    int cb_act;     // offset 0xC, size 0x4
+    int cb_blood;   // offset 0x10, size 0x4
+    int dw_act;     // offset 0x14, size 0x4
+    int dw_blood;   // offset 0x18, size 0x4
+} EN26_WPNDAMAGE_WORK;
+
+typedef struct EN26_FIRE_WORK
+{
+    // total size: 0x18
+    int obj;        // offset 0x0, size 0x4
+    float size;     // offset 0x4, size 0x4
+    float len;      // offset 0x8, size 0x4
+	NJS_POINT3 ofs; // offset 0xC, size 0xC
+} EN26_FIRE_WORK;
+
+typedef struct EN26_KAMI_WORK
+{
+    // total size: 0xC
+    int mtn_no;     // offset 0x0, size 0x4
+    short frm[4];   // offset 0x4, size 0x8
+} EN26_KAMI_WORK;
+
+typedef struct EN26_FMTN_WORK
+{
+    // total size: 0xC
+	int type;   // offset 0x0, size 0x4
+	int s_frm;  // offset 0x4, size 0x4
+	int e_frm;  // offset 0x8, size 0x4
+} EN26_FMTN_WORK;
+
+typedef struct EN26_ATB_WORK
+{
+    // total size: 0x8
+	int frm;            // offset 0x0, size 0x4
+	unsigned int act;   // offset 0x4, size 0x4
+} EN26_ATB_WORK;
+
+typedef struct EN26_MTN_WORK
+{
+    // total size: 0x28
+	int no;                 // offset 0x0, size 0x4
+	EN26_FMTN_WORK fmtn[3]; // offset 0x4, size 0x24
+} EN26_MTN_WORK;
+
+typedef struct EN26_MTN2_WORK
+{
+    // total size: 0x24
+	int no;                 // offset 0x0, size 0x4
+	EN26_ATB_WORK atb[4];   // offset 0x4, size 0x20
+} EN26_MTN2_WORK;
 
 void bhEne26(BH_PWORK* epw);
 void bhEne26_DmgCheck(BH_PWORK* epw);
-/*void bhEne26_DamageAdd(BH_PWORK* epw, _anon25* wp_tbl);*/
+void bhEne26_DamageAdd(BH_PWORK* epw, EN26_WPNDAMAGE_WORK* wp_tbl);
 void bhEne26_LinkFireEffect(BH_PWORK* epw, int type);
-/*void bhEne26_DmgCheckType00(BH_PWORK* epw, _anon25* wp_tbl);*/
+void bhEne26_DmgCheckType00(BH_PWORK* epw,  EN26_WPNDAMAGE_WORK* wp_tbl);
 int bhEne26_CheckExpHead(BH_PWORK* epw);
-/*void bhEne26_SetBlood(BH_PWORK* epw, _anon25* wp_tbl);*/
+void bhEne26_SetBlood(BH_PWORK* epw, EN26_WPNDAMAGE_WORK* wp_tbl);
 void bhEne26_MainLoop(BH_PWORK* epw, BH_PWORK* pl);
 void bhEne26_PlayerControl(BH_PWORK* epw, BH_PWORK* pl);
 int bhEne26_SetMtn(BH_PWORK* epw);
@@ -49,7 +107,7 @@ void bhEne26_Die(BH_PWORK* epw);
 void bhEne26_DDType(BH_PWORK* epw);
 void bhEne26_DD00(BH_PWORK* epw);
 int bhEne26_EatCheck(BH_PWORK* epw, int rng, float dist, int mode);
-void bhEne26_ExpHeadEffect(BH_PWORK* epw);
+void bhEne26_ExpHeadEffect(BH_PWORK* epw, int unused); // different from DWARF
 void bhEne26_NeckBloodEffect(BH_PWORK* epw, int type);
 void bhEne26_KamiEffect(BH_PWORK* epw, int frm);
 void bhEne26_SePlay(BH_PWORK* epw, int no);
